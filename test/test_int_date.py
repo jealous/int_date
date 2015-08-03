5655ba2707cdc3c06d20a9b3dd3a184d5d7cc8e0
Cedric Zhuang <cedric.zhuang@gmail.com>
2015-08-04 01:44:16 +0800

    Add more tests and re-organize interfaces
    Integrate multiple convert functions into api:
        to_int_date(the_day)
    
    Add utility functions like in_date and in_year.
    Refactor the tests.

--- test/test_int_date.py
+++ test/test_int_date.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import datetime
 from hamcrest import assert_that, equal_to, greater_than
 import int_date
 
@@ -12,9 +13,17 @@ class TestIntDate(TestCase):
         assert_that(date.month, equal_to(5))
         assert_that(date.day, equal_to(15))
 
+    def test_get_date_from_int_error(self):
+        with self.assertRaises(ValueError):
+            int_date.get_date_from_int(20120532)
+
     def test_get_day_interval(self):
         interval = int_date.get_int_day_interval(20120530, 20120602)
         assert_that(interval, equal_to(3))
+
+    def test_get_day_interval_negative(self):
+        interval = int_date.get_int_day_interval(20120503, 20120324)
+        assert_that(interval, equal_to(-40))
 
     def test_get_date_from_diff_before(self):
         assert_that(int_date.get_date_from_diff(20120503, -40),
@@ -28,10 +37,46 @@ class TestIntDate(TestCase):
         assert_that(int_date.get_date_from_diff(20120228, 2),
                     equal_to(20120301))
 
+    def test_to_int_date(self):
+        date = int_date.to_int_date(datetime.datetime(2015, 5, 21))
+        assert_that(date, equal_to(20150521))
+
+    def test_to_int_date_str_1(self):
+        date = int_date.to_int_date('2015-1-3')
+        assert_that(date, equal_to(20150103))
+
+    def test_to_int_date_str_2(self):
+        date = int_date.to_int_date('2015/11/23')
+        assert_that(date, equal_to(20151123))
+
+    def test_to_int_date_str_error(self):
+        with self.assertRaises(ValueError):
+            int_date.to_int_date('20151301')
+
+    def test_date_to_int_none(self):
+        with self.assertRaises(ValueError):
+            int_date.to_int_date(None)
+
     def test_in_month(self):
         assert_that(int_date.in_month(20140503, [3, 5, 7]), equal_to(True))
+        assert_that(int_date.in_month(20140503, {3, 5, 7}), equal_to(True))
+        assert_that(int_date.in_month(20140503, (3, 5, 7)), equal_to(True))
         assert_that(int_date.in_month(20140131, [3, 5, 7]), equal_to(False))
+        assert_that(int_date.in_month(20140131, 1), equal_to(True))
+        assert_that(int_date.in_month(20140131, 3), equal_to(False))
 
-    def test_get_today(self):
-        today = int_date.get_today()
+    def test_in_date(self):
+        assert_that(int_date.in_date(20140503, [3, 5, 7]), equal_to(True))
+        assert_that(int_date.in_date(20140503, {1, 5, 7}), equal_to(False))
+        assert_that(int_date.in_date(20140131, 31), equal_to(True))
+        assert_that(int_date.in_date(20140131, 30), equal_to(False))
+
+    def test_in_year(self):
+        assert_that(int_date.in_year(20140503, [2013, 2014]), equal_to(True))
+        assert_that(int_date.in_year(20140503, {2013, 2015}), equal_to(False))
+        assert_that(int_date.in_year(20140131, 2014), equal_to(True))
+        assert_that(int_date.in_year(20140131, 2013), equal_to(False))
+
+    def test_today(self):
+        today = int_date.today()
         assert_that(today, greater_than(20150504))
